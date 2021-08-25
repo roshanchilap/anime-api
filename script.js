7b9e3c8c6370403d0dd204833e059fbcd62ad8d4
Roshan Chilap <roshanchilap143@gmail.com>
2021-08-25 12:46:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-const api_url = "https://api.jikan.moe/v3"
+const api_url = "https://api.jikan.moe/v3";
 
 async function searchAnime(event){
 
     event.preventDefault();
     const form = new FormData(this)
-    
-    const query = form.get('search')
-
-    console.log(query)
-
-    fetch(`${api_url}/search/anime?q=${query}&page=1`)
-    .then(res =>res.json())
-//    .then(data=>console.log(data))
-    .then(updateDom)
-    
-    .catch(err =>console.warn(err.message))
+    let query = form.get('search');
+    let res = await fetch(`${api_url}/search/anime?q=${query}&page=1`)
+    console.log(res)
+    if(res.ok)
+    {
+    const data = await res.json();
+    console.log(data)
+    showAnime(data)
+    resetForm();
+    }
+    else if (res.ok==false)
+    {
+        alert("Try something new...!!!");
+        resetForm();
+        
+    }
+        
 }
 
-function updateDom(data){
+function resetForm(){
+    document.getElementById('search').value="";
+}
+
+function showAnime(data){
 const searchResults = document.getElementById('search-results');
-searchResults.innerHTML= data.results.map(anime=>{
+searchResults.innerHTML= data.results.map(data=>{
     return `
     <div class="card" style="width: 18rem;">
-  <img src="${anime.image_url}" class="card-img-top" alt="...">
+  <img src="${data.image_url}" class="card-img-top" alt="...">
   <div class="card-body">
-    <h2 class="card-title">${anime.title}</h2>
-    <p class="card-text">${anime.synopsis}</p>
-    <p class="card-type">${anime.type}</p>
-    <p class="card-rating">IMDB rating: ${anime.score}</p>
-    <p class="card-sdate">Start date: ${new Date(anime.start_date).toDateString()}</p>
-    <p class="card-edate">End date: ${new Date(anime.end_date).toDateString()}</p>
+    <h2 class="card-title">${data.title}</h2>
+    <p class="card-text"><q>${data.synopsis}<q></p>
+    <p class="card-type"><span class="sp1">Type:</span> ${data.type}</p>
+    <p class="card-rating"><span class="sp1">IMDB rating:</span> ⭐${data.score}</p>
+    <p class="card-sdate"><span class="sp1">Start date:</span> ${new Date(data.start_date).toDateString()}</p>
+    <p class="card-edate"><span class="sp1">End date:</span> ${new Date(data.end_date).toDateString()}</p>
   </div>
 </div>
     `
@@ -37,9 +47,7 @@ searchResults.innerHTML= data.results.map(anime=>{
 }
 
 
-function pageLoaded(){
+function loadAnime(){
     const form = document.getElementById('search-form');
     form.addEventListener('submit', searchAnime)
 }
-
-window.addEventListener("load", pageLoaded)

d000f8c8820c8b4a815a7ba9042fdf7fe9b91156
Roshan Chilap <roshanchilap143@gmail.com>
2021-12-22 11:03:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const api_url = "https://api.jikan.moe/v3";
 let query = "Naruto";
-  let res = await fetch(`${api_url}/search/anime?q=${query}&page=1`);
+  let res = fetch(`${api_url}/search/anime?q=${query}&page=1`);
   console.log(res);
   if (res.ok) {
-    const data = await res.json();
+    const data = res.json();
     console.log(data);
     showAnime(data);
     resetForm();
